5a8761233062aa7a8c41f9c789350ffd85881c2a
taif <taifbakshi.dev@gmail.com>
2020-10-05 11:31:00 +0600

    seperated frontend and backend folder

--- frontend/src/Components/Navbar/Navbar.component.jsx
+++ frontend/src/Components/Navbar/Navbar.component.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
     return (
         
             
-        <header className="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+        <header className="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l clearfix">
             
         <nav className="f6 fw7 ttu tracked">
             <Link className="link dim white dib2 mr3" to="/" title="Home">Home</Link>
             <Link className="link dim white dib2 mr3" to="/quiz" title="About">Quiz</Link>
             <Link className="link dim white dib2 mr3" to="/admin" title="Store">Login</Link>            
-            <Link className="link dim white dib2 mr3" to="/" title="Contact">Contact</Link>
+            <Link className="link dim white dib2 mr3" to="/contact" title="Contact">Contact</Link>
             <Link className="link dim white dib2 mr3" to="/leadboard" title="Store">Leadboard</Link>
         </nav>
         </header>
